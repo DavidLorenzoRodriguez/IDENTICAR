6208b2f39ddf68ec154d2c4b91265e8b0ab6e3ef
David Lorenzo Rodríguez <dlorenzo@mrfs.lan>
2023-04-20 20:53:17 +0200

    carrusel
    con la opcion de carrusel

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,45 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+const carousel = document.querySelector('.carousel');
+const carouselItems = document.querySelector('.carousel-items');
+const images = document.querySelectorAll('.carousel-items img');
+
+let currentIndex = 0;
+const slideWidth = carousel.clientWidth / images.length;
+
+
+// Agregamos la propiedad 'left' para mover los elementos de la lista a la izquierda
+images.forEach((img, i) => {
+  img.parentElement.style.left = `${i * slideWidth}px`;
+});
+
+// Función que se encarga de avanzar al siguiente elemento
+function slideNext() {
+  if (currentIndex === images.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  carouselItems.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+// Función que se encarga de retroceder al elemento anterior
+function slidePrev() {
+  if (currentIndex === 0) {
+    currentIndex = images.length - 1;
+  } else {
+    currentIndex--;
+  }
+  carouselItems.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+// Agregamos los listeners para los botones de avanzar y retroceder
+document.querySelector('.next-button').addEventListener('click', slideNext);
+document.querySelector('.prev-button').addEventListener('click', slidePrev);
+
+
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
