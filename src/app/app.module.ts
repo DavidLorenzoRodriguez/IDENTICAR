8cbdbb00760f57a3f6d7a42cea7678365402ce8f
Focas <fcocasasg@hotmail.com>
2023-04-25 09:40:08 +0200

    nuevos carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuBarComponent } from './shared/menu-bar/menu-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuBarComponent
-  ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
